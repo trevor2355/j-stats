3e0fe208a3c8c14b4e2fe3246217cfad7f7aa73a
Trevor Bell <trevor2355@gmail.com>
2020-04-24 18:56:40 -0600

    changed connection to a pool

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 const config = require('./config.js');
 
-const connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || config);
+const connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL || config);
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// });
 
 module.exports = connection;
